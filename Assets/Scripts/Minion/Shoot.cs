7d90cf6bdb459ecd38e8df3e15a5b981ef8626df
Shlmp <daniel.kil@uniat.edu.mx>
2024-09-21 19:28:44 -0600

    Add Feature: Minions
    Added minion prefab, bullet prefab & EnemySpawner prefab. Minions can spawn, shoot, move towards player & deal damage.

---
+++ Assets/Scripts/Minion/Shoot.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform player;
+    public Rigidbody bullet;
+    public Transform spawner;
+    public float bulletSpeed;
+    bool isShooting = false;
+
+    void Start()
+    {
+        StartCoroutine(shoot());
+        player = PlayerStats.instance.transform;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 distance = this.transform.position - player.transform.position;
+        if (Physics.Linecast(this.transform.position, player.transform.position, out hit, -1))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                if(distance.magnitude > 10) // Enemy's range
+                {
+                    Debug.Log("Enemy Approaching");
+                    this.transform.Translate(Vector3.forward * 2f * Time.deltaTime);
+                    this.transform.LookAt(player.transform);
+                    isShooting = false;
+                    StartCoroutine(enemyLife());
+                }
+                else
+                {
+                    isShooting = true;
+                    this.transform.LookAt(player.transform);
+                }
+            }
+        }
+    }
+
+    IEnumerator shoot()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (isShooting)
+        {
+            Debug.Log("Enemy Shooting");
+            Rigidbody clone;
+            clone = (Rigidbody)Instantiate(bullet, spawner.transform.position, Quaternion.identity);
+            clone.velocity = spawner.TransformDirection(Vector3.forward * bulletSpeed * Time.deltaTime);
+        }
+        StartCoroutine(shoot());
+    }
+
+    IEnumerator enemyLife()
+    {
+        yield return new WaitForSeconds(3f);
+        Debug.Log("Destroying Enemy");
+        Destroy(gameObject);
+    }
+}
